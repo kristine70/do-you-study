06faf6f3cd7d55b8f9b910813de9191100c0b5ab
Kristine70 <kristine.liang@outlook.com>
2025-04-17 15:14:28 -0700

    refactor(message): simplify sendMessage function by removing bot parameter and fetching channel directly

--- src/services/message.ts
+++ src/services/message.ts
@@ -1,17 +1,11 @@
-import {Client, TextChannel} from 'discord.js';
+import {TextChannel} from 'discord.js';
 import config from '../config';
 import {logger} from '../utils/logger';
 
-const CHANNEL_ID = config.MANAGER_CHANNEL_ID;
-
 //* pass test
-export const sendMessage = async (bot: Client, userIds: string[]) => {
+export const sendMessage = async (channel: TextChannel, userIds: string[]) => {
   try {
-    const channel = await bot.channels.fetch(CHANNEL_ID);
-    if (!(channel instanceof TextChannel)) throw new Error(`Channel ${channel?.id} is not valid`);
-
     const content = config.MESSAGE_CONTENT(userIds);
-
     await channel.send({content, allowedMentions: {users: userIds}});
   } catch (error) {
     logger.error('[SEND MESSAGE] fail:', error);
